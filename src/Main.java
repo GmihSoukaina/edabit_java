3bc0aeab53b6ddcc3a8b7d174189a041cd973edb
GmihSoukaina <soukaina4125@gmail.com>
2025-01-15 09:50:27 +0100

    Primitive Types vs Reference Types

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,5 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        int maxage = 30;
-        int age = maxage;
-        System.out.println(age);
-
-
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
     }
 }
